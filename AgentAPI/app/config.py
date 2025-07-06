ccceb686f3e11c46be901b1a7dec81d57039109e
Gaurav <gauravs260668@gmail.com>
2025-07-05 19:49:03 -0500

    updated the docker

--- AgentAPI/app/config.py
+++ AgentAPI/app/config.py
@@ -6,7 +6,7 @@ class Config:
     OPENAI_BASE_URL: str = "https://llm.gauravshivaprasad.com/v2"
     OPENAI_API_KEY: str = "sk-372c69b72fb14a90a2e1b0b17884d9b4"
     MODEL_PROVIDER: str = "openai"
-    
+        
     MEDIA_DESCRIPTION_PROMPT: str = "Provide an extremely detailed description of this media content. Include every visible/audible element, text, object, person, color, layout, sounds, speech, and any other relevant details without missing anything."
 
 config = Config()
