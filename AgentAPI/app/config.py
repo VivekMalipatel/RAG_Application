0eb8bda7c123e3edf8bd3666e8169d96e6c30572
Gaurav <gauravs260668@gmail.com>
2025-07-16 21:29:12 -0500

    updated env in docker

--- AgentAPI/app/config.py
+++ AgentAPI/app/config.py
@@ -6,18 +6,18 @@ class Config:
     REASONING_LLM_MODEL: str = os.getenv("REASONING_LLM_MODEL")
     VLM_MODEL: str = os.getenv("VLM_MODEL")
     MULTIMODEL_EMBEDDING_MODEL: str = os.getenv("MULTIMODEL_EMBEDDING_MODEL")
-    MULTIMODEL_EMBEDDING_MODEL_DIMS: int = int(os.getenv("MULTIMODEL_EMBEDDING_MODEL_DIMS"))
+    MULTIMODEL_EMBEDDING_MODEL_DIMS: int = int(os.getenv("MULTIMODEL_EMBEDDING_MODEL_DIMS",2048))
     TEXT_EMBEDDING_MODEL: str = os.getenv("TEXT_EMBEDDING_MODEL")
-    TEXT_EMBEDDING_MODEL_DIMS: int = int(os.getenv("TEXT_EMBEDDING_MODEL_DIMS"))
+    TEXT_EMBEDDING_MODEL_DIMS: int = int(os.getenv("TEXT_EMBEDDING_MODEL_DIMS", 768))
     OPENAI_BASE_URL: str = os.getenv("OPENAI_BASE_URL")
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
     MODEL_PROVIDER: str = os.getenv("MODEL_PROVIDER")
 
     # Redis Configuration
-    REDIS_HOST: str = os.getenv("REDIS_HOST")
-    REDIS_PORT: int = int(os.getenv("REDIS_PORT"))
-    REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD")
-    REDIS_DB: int = int(os.getenv("REDIS_DB"))
+    REDIS_HOST: str = os.getenv("REDIS_HOST", "192.168.0.20")
+    REDIS_PORT: int = int(os.getenv("REDIS_PORT", 6379))
+    REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD", "your_redis_password")
+    REDIS_DB: int = int(os.getenv("REDIS_DB", 0))
 
     # Prompt Configuration
     MEDIA_DESCRIPTION_PROMPT: str = os.getenv(
